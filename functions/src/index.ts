ba4de5ac13839a679eb4046bd67f93b7823d719d
rettusm <rettusm@wwu.edu>
2023-04-25 13:54:36 -0700

    randomly selected word

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,22 @@
-import * as functions from "firebase-functions";
-
+import functions = require("firebase-functions");
+import admin = require("firebase-admin");
+import fs = require("fs")
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+let wordsCom = "";
+fs.promises.readFile("Words.txt").then((res) => {
+  wordsCom = res.toString().trim();
+}).catch((err) => {
+  functions.logger.info(err);
+});
+
+
+export const words = functions.https.onCall(() => {
+  while (wordsCom === null) {
+    // this is bad but it will do.
+  }
+  const wordArr = wordsCom.split("\r\n");
+  return wordArr[Math.floor(Math.random() * wordArr.length)];
+});
+
