c046e0473f689bbce288745c844bc51d0cb62520
rettusm <rettusm@wwu.edu>
2023-04-25 21:32:56 -0700

    functioning game

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -10,8 +10,9 @@ const firebaseConfig = {
   appId: "1:908221585980:web:8b90754eebd4485ae3c139",
   measurementId: "G-ENM8KEH2CC"
 };
-let wrongCount = 0;
+
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const functions = getFunctions();
 // const auth = getAuth(app);
@@ -25,24 +26,112 @@ export function pcPlay(){
     challengeDisplay();
   })
 }
+let wrongCount = 0;//tracks the current words wrong guess count
+let guesses = [];/*Maintains all guessed letters any repeats
+are automatically scored as incorrect.*/
+let correctCount = 0;
 function challengeDisplay(){
   wrongCount = 0;
+  correctCount = 0;
+  guesses = [];
   let chall = document.getElementById("challenge");
   removeAllChildNodes(chall);
   chall.innerText = "Your word to solve";
-
-  for(let i = 1; i <= curWord.length; i++){
+  for(let i = 0; i < curWord.length; i++){
     let li = document.createElement("p");
     li.setAttribute("id", "list" + i);
-    li.style.listStyle = "none";
     li.style.display = "inline";
     li.innerText = "_";
     chall.appendChild(li);
   }
   document.getElementById("guessForm").style.display = "block"
+  document.getElementById("resultField").style.display = "block"
 }
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
       parent.removeChild(parent.firstChild);
   }
+}
+export function guessForm(e){
+  e.preventDefault();
+  const data = document.getElementById("guessForm");
+  let temp = data["letter"].value;
+  if(validation(temp)){
+    temp = temp.toLowerCase();
+    console.log("valid");
+  } else {
+    data.reset()
+    return;
+  }
+  if(guesses.includes(temp)){
+    wrongCount++;
+    data.reset()
+    botUpdate();
+  } else if(!charMatch(temp)){
+    wrongCount++;
+    guesses.push(temp);
+    data.reset()
+    guessedLetters();
+    botUpdate();
+  } else {
+    guesses.push(temp);
+    data.reset()
+    guessedLetters();
+  }
+}
+function validation(char){
+  const letters = new RegExp(/^[A-Za-z]+$/);
+  if(char.length !== 1){
+    alert("One Character Must Be Submitted!")
+    wrongCount++;
+    botUpdate();
+    return false;
+  }
+  if(!char.match(letters)){
+    alert("Only Letter Inputs Are Valid");
+    wrongCount++;
+    botUpdate();
+    return false;
+  }
+  return true;
+}
+function botUpdate(){
+  console.log(wrongCount);
+  if(wrongCount > 5){
+    document.getElementById("guessForm").style.display = "none";
+    document.getElementById("lost").style.display = "block";
+    document.getElementById("resultField").style.display = "none"
+  }
+}
+function guessedLetters(){
+  let gl = document.getElementById("guessedLetters");
+  removeAllChildNodes(gl);
+  gl.innerText = "Letters you've guessed: ";
+  for(let i = 0; i < guesses.length; i++){
+    let li = document.createElement("p");
+    li.setAttribute("id", "letter" + i);
+    li.style.display = "inline";
+    li.innerText = guesses[i];
+    gl.appendChild(li);
+  }
+}
+function charMatch(char){
+  let match = false;
+  for(let i = 0; i < curWord.length; i++){
+    if(char === curWord[i]){
+      correctCount++;
+      document.getElementById("list" + i).innerText = char;
+      match = true;
+      if(correctCount === curWord.length){
+        win();
+      }
+    }
+  }
+  return match;
+}
+function win(){
+  document.getElementById("guessForm").style.display = "none";
+  document.getElementById("lost").innerText = "You Won!!"
+  document.getElementById("lost").style.display = "block";
+  document.getElementById("resultField").style.display = "none"
 }
